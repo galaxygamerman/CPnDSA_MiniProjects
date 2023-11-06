bc16af00bf5c0891466ef3828063a1a158a82781
M Imaad Iqbal <64598842+galaxygamerman@users.noreply.github.com>
2023-11-06 15:25:02 +0530

    Formatted using c/c++ extension

--- EncodeDecode.cpp
+++ EncodeDecode.cpp
@@ -1,61 +1,66 @@
 #include <iostream>
 using namespace std;
-void encode(char*,int);
-void decode(char*,int);
+
+void encode(char *, int);
+void decode(char *, int);
 
 int main()
 {
-    char whichFunc[6],message[20];
+    char whichFunc[6], message[20];
     int shift;
-    
-    cout<<"Type encode or decode to perform said function: "; cin >> whichFunc;
-    cout<<"Input the Message: "; cin >> message;
-    cout<<"Input the shift: "; cin >> shift;
-    
-    if(*whichFunc=='e' || *whichFunc=='E')
+
+    cout << "Type encode or decode to perform said function: ";
+    cin >> whichFunc;
+    cout << "Input the Message: ";
+    cin >> message;
+    cout << "Input the shift: ";
+    cin >> shift;
+
+    if (*whichFunc == 'e' || *whichFunc == 'E')
     {
-        cout<<"The Encoded message: ";
-        encode(message,shift);
+        cout << "The Encoded message: ";
+        encode(message, shift);
     }
-    else if(*whichFunc=='d' || *whichFunc=='D')
+    else if (*whichFunc == 'd' || *whichFunc == 'D')
     {
-        cout<<"The Decoded message: ";
-        decode(message,shift);
+        cout << "The Decoded message: ";
+        decode(message, shift);
     }
     else
     {
-        cout<<"Wrong Input!"; return 0;
+        cout << "Wrong Input!";
+        return 0;
     }
-    cout<<message;
+    cout << message;
     return 0;
 }
 
-void encode(char *m,int s)
+void encode(char *m, int s)
 {
     while (*m != '\0')
     {
         for (int j = 0; j < s; j++)
         {
             (*m)++;
-            if((*m)>'z')
-                (*m)='a';
-            else if((*m)>'Z' && (*m)<'a')
-                (*m)='A';
+            if ((*m) > 'z')
+                (*m) = 'a';
+            else if ((*m) > 'Z' && (*m) < 'a')
+                (*m) = 'A';
         }
         m++;
     }
 }
-void decode(char *m,int s)
+void decode(char *m, int s)
 {
     while (*m != '\0')
     {
         for (int j = 0; j < s; j++)
         {
             (*m)--;
-            if((*m)<'A')
-                (*m)='Z';
-            else if((*m)>'Z' && (*m)<'a')
-                (*m)='z';
+            if ((*m) < 'A')
+                (*m) = 'Z';
+            else if ((*m) > 'Z' && (*m) < 'a')
+                (*m) = 'z';
         }
         m++;
     }
--- PassGen.cpp
+++ PassGen.cpp
@@ -1,66 +1,73 @@
-#include<iostream>
-using std::cout;
+#include <iostream>
 using std::cin;
+using std::cout;
 using std::endl;
 
-//This structure exists only because I felt fancy, and didn't want to use "_" in variable names
+// This structure exists only because I felt fancy, and didn't want to use "_" in variable names
 struct digitData
 {
-    int desiredDigits,printedDigits=0;
-}alphas,nums,symbo;
+    int desiredDigits, printedDigits = 0;
+} alphas, nums, symbo;
 
 int totalDigits;
-char alphaKeySet[]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-     numKeySet[]="0123456789",
-     symboKeySet[]="!@#$%^&*()+-*/",
+char alphaKeySet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+     numKeySet[] = "0123456789",
+     symboKeySet[] = "!@#$%^&*()+-*/",
      password[32];
 
 int main()
 {
     srand(time(NULL));
-    cout<<"Input the no. of letters in your password: "; cin>> alphas.desiredDigits;
-    cout<<"Input the no. of numbers in your password: "; cin>> nums.desiredDigits;
-    cout<<"Input the no. of symbols in your password: "; cin>> symbo.desiredDigits;
+    cout << "Input the no. of letters in your password: ";
+    cin >> alphas.desiredDigits;
+    cout << "Input the no. of numbers in your password: ";
+    cin >> nums.desiredDigits;
+    cout << "Input the no. of symbols in your password: ";
+    cin >> symbo.desiredDigits;
 
-    totalDigits=alphas.desiredDigits+nums.desiredDigits+symbo.desiredDigits; //To calculate how long the desired password is
+    totalDigits = alphas.desiredDigits + nums.desiredDigits + symbo.desiredDigits; // To calculate how long the desired password is
 
+    cout << "Your password of " << totalDigits << " digits, sire!: \n";
 
-    cout<<"Your password of "<<totalDigits<<" digits, sire!: \n";
+    int i = 0;
+labelWhichType: // A label to allow me to loop the program according to how I intended. Trust me, for and while didn't work well
+    switch (rand() % 3 + 1)
+    {
+        // Aphabets
+    case 1:
+        if (alphas.printedDigits >= alphas.desiredDigits)
+            goto labelWhichType;
+        password[i] = alphaKeySet[rand() % 53];
+        alphas.printedDigits++;
+        cout << password[i];
+        break;
 
-    int i=0;
-    labelWhichType:  //A label to allow me to loop the program according to how I intended. Trust me, for and while didn't work well
-        switch (rand()%3 +1)
-        {
-            //Aphabets
-        case 1:
-            if(alphas.printedDigits>=alphas.desiredDigits) goto labelWhichType;
-            password[i]=alphaKeySet[rand()%53];
-            alphas.printedDigits++;
-            cout<<password[i];
-            break;
+        // Numbers
+    case 2:
+        if (nums.printedDigits >= nums.desiredDigits)
+            goto labelWhichType;
+        password[i] = numKeySet[rand() % 11];
+        nums.printedDigits++;
+        cout << password[i];
+        break;
 
-            //Numbers
-        case 2:
-            if(nums.printedDigits>=nums.desiredDigits) goto labelWhichType;
-            password[i]=numKeySet[rand()%11];
-            nums.printedDigits++;
-            cout<<password[i];
-            break;
+        // Symbols
+    case 3:
+        if (symbo.printedDigits >= symbo.desiredDigits)
+            goto labelWhichType;
+        password[i] = symboKeySet[rand() % 15];
+        symbo.printedDigits++;
+        cout << password[i];
+        break;
 
-            //Symbols
-        case 3:
-            if(symbo.printedDigits>=symbo.desiredDigits) goto labelWhichType;
-            password[i]=symboKeySet[rand()%15];
-            symbo.printedDigits++;
-            cout<<password[i];
-            break;
+        // Incase anything went wrong
+    default:
+        cout << "[Debug for Dev]Wrong rand value in loop" << i << endl;
+    }
+    i = alphas.printedDigits + nums.printedDigits + symbo.printedDigits;
 
-            //Incase anything went wrong
-        default: cout<<"[Debug for Dev]Wrong rand value in loop"<<i<<endl;
-        }
-        i=alphas.printedDigits+nums.printedDigits+symbo.printedDigits;
+    if (i < totalDigits)
+        goto labelWhichType; // To loop back
 
-    if(i<totalDigits) goto labelWhichType; //To loop back
-    
     return 0;
 }
